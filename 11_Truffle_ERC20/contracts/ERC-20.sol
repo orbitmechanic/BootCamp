d1ca499df5669bb889616ace6ba8856754a9aaf3
Orbit Mechanic <56613757+orbitmechanic@users.noreply.github.com>
2020-10-17 16:56:24 -0700

    Roughing in truffle ERC20 project

---
+++ 11_Truffle_ERC20/contracts/ERC-20.sol
@@ -0,0 +1,54 @@
+pragma solidity 0.7.0;
+import './Ownable.sol';
+// SPDX-License-Identifier: UNLICENSED
+
+contract ERC20 is Ownable {
+
+    mapping (address => uint256) private _balances;
+
+    uint256 private _totalSupply;
+
+    string private _name;
+    string private _symbol;
+    uint8 private _decimals;
+
+        constructor (string memory name, string memory symbol) public {
+        _name = name;
+        _symbol = symbol;
+        _decimals = 18;
+    }
+
+
+    function name() public view returns (string memory) {
+        return _name;
+    }
+
+    function symbol() public view returns (string memory) {
+        return _symbol;
+    }
+
+    function decimals() public view returns (uint8) {
+        return _decimals;
+    }
+
+    function totalSupply() public view returns (uint256) {
+        return _totalSupply;
+    }
+
+    function balanceOf(address account) public view returns (uint256) {
+        return _balances[account];
+    }
+
+    function mint(address account, uint256 amount) public onlyOwner {
+        // tokenomics: Owner go brrrr
+        _totalSupply += amount;
+        _balances[account] += amount; 
+    }
+
+    function transfer(address recipient, uint256 amount) public returns (bool) {
+        require(_balances[msg.sender] >= amount,'Insufficient funds available.');
+        _balances[msg.sender] -= amount;
+        _balances[recipient] += amount;
+        return true; // per spec.
+    }
+}
